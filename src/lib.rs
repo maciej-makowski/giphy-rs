975ce3b1889a078a9ffbfdb5a8ec5bd1c7199fb4
Maciej Makowski <maciejm.githubatcfiet.net>
2019-03-31 23:51:15 +0100

    Better crate-level docs, added examples

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,87 @@
 //! Stickers and posting GIFs to [Giphy] is currently not supported.
 //! 
 //! ## Examples
+//! ### Synchronous API
+//! Create a synchronous [`reqwest::Client`] and [`SyncApi`] object holding your API key
 //! 
+//! ```
+//! use giphy::v1::sync::SyncApi;
 //! 
+//! let client = reqwest::Client::new();
+//! let api = SyncApi::new("[your Giphy API key]", client);
+//! ```
+//! 
+//! Then create and customise an appropriate request object from [`giphy::v1::gifs`] module
+//! to make the request.
+//! 
+//! For example, to search for gifs related to "tacos"
+//! 
+//! ```
+//! # use giphy::v1::sync::SyncApi;
+//! use giphy::v1::gifs::SearchRequest;
+//! # let client = reqwest::Client::new();
+//! # let api = SyncApi::new("[your Giphy API key]", client);
+//! 
+//! let response = SearchRequest::new("tacos")  // Create search request
+//!     .with_limit(10)                         // Limit the number of objects in response
+//!     .send_to(&api)                          // Send the request using SyncApi
+//!     .unwrap_or_else(|e|                     // Unwrap the response Result
+//!         panic!("Search request failed: {:?}", e)
+//!     );
+//! 
+//! let first_result = response.data            // response.data will contain GIFs data
+//!     .first()
+//!     .unwrap_or_else(||
+//!         panic!("No results returned from Giphy")
+//!     );
+//! ```
+//! 
+//! ### Asynchronous API
+//! Create an asynchronous [`reqwest::async::Client`] and [`AsyncApi`] object holding your API key
+//! 
+//! ```
+//! use giphy::v1::r#async::AsyncApi;
+//! 
+//! let client = reqwest::r#async::Client::new();
+//! let api = AsyncApi::new("[your Giphy API key]", client);
+//! ```
+//! 
+//! The request object is created the same way as when using the synchronous API. The
+//! only difference is, [`send_to`] method will return a [`Future`] object when called
+//! with [`AsyncApi`].
+//! 
+//! ```
+//! # use giphy::v1::r#async::AsyncApi;
+//! use giphy::v1::gifs::SearchRequest;
+//! # let client = reqwest::r#async::Client::new();
+//! # let api = AsyncApi::new("[your Giphy API key]", client);
+//! // Create search request the same way as with synchronous api
+//! let search_fut = SearchRequest::new("tacos")
+//!     .with_limit(10)
+//!     .send_to(&api) // Send the request using AsyncApi, this returns a Future
+//!     .map(|reponse| {
+//!         // Operate on response
+//!         println!("Response: {:?}", response);
+//!     });
+//! ```
+//! The `search_fut` can then be run with [`tokio`]. 
+//! 
+//! ### More examples
+//! See [examples] for showcase of all the currently possible [Giphy HTTP API]
+//! requests in both synchronous and asynchronous style.
 //!
 //! [Giphy]: https://giphy.com
 //! [Giphy API]: https://developers.giphy.com/docs/
 //! [Giphy HTTP API]: https://developers.giphy.com/docs/
 //! [`reqwest`]: https://docs.rs/reqwest
 //! [`v1`]: ./v1/index.html
+//! [`reqwest::Client`]: ../reqwest/struct.Client.html
+//! [`reqwest::async::Client`]: ../reqwest/async/struct.Client.html
+//! [`SyncApi`]: v1/sync/struct.SyncApi.html
+//! [`AsyncApi`]: v1/async/struct.AsyncApi.html
+//! [`giphy::v1::gifs`]: v1/gifs/index.html
+//! [examples]: https://github.com/cfiet/giphy-rs/tree/master/examples
+//! [`tokio`]: ../tokio/index.html
 
 extern crate futures;
 extern crate reqwest;
